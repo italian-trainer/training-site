d73c603e6a9033961317b0c87e898bbb624daae4
Cristian <cyanez@ucla.edu>
2025-02-14 14:36:35 -0800

    Updated Homepage
    Imported the actual logo from Marias Italian kitchen, will update this page further to want to get a working template

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Home.css'; 
-import Logo from '../src/homeLogo.png'
+import Logo from '../src/mikLogo.png'
 
 const HomePage = () => {
   return (
     <div className="homepage-container">
-      <h1>Maria's Italian Restaurant</h1>
+      <h1>Welcome to Maria's Italian Kitchen Training Course</h1>
       <div className="logo-container">
         <img 
           src={Logo} // Use the imported Logo here
@@ -13,6 +13,7 @@ const HomePage = () => {
           className="logo" 
         />
       </div>
+
       <br/>
       <button className="login-button">Login</button>
     </div>
