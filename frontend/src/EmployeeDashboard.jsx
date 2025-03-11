076c47a86b854a183028ef5c6f9669440e5f3f17
Yonatan Ahituv <ahituvyonatan@gmail.com>
2025-03-10 23:09:10 -0700

    Add employee dash

--- frontend/src/EmployeeDashboard.jsx
+++ frontend/src/EmployeeDashboard.jsx
@@ -1,10 +1,70 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const EmployeeDashboard = () => {
+  const [trainings, setTrainings] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchTrainings = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:5005/trainings/get_trainings",
+          {
+            method: "GET",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          }
+        );
 
+        if (!response.ok) throw new Error("Failed to fetch trainings");
+
+        const data = await response.json();
+        console.log("Data:", data.data);
+
+        setTrainings(data.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching trainings:", error);
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+
+    fetchTrainings();
+  }, []);
   return (
-    <h1>Employee Dashboard</h1>
-  )
+    <>
+      <h1>Employee Dashboard</h1>
+      <section className="assigned-trainings">
+        <h3>Assigned Trainings</h3>
+        {loading ? (
+          <p>Loading trainings...</p>
+        ) : error ? (
+          <p className="error">{error}</p>
+        ) : trainings.length > 0 ? (
+          <div className="training-list">
+            {trainings.map((training) => (
+              <div key={training.id} className="training-card">
+                <h4 className="training-title">{training.training}</h4>
+                <p className="training-description"></p>
+                <p className="training-description">
+                  {(training.complete && "Complete") ||
+                    Math.floor(
+                      (training.current_page / training.total_pages) * 100
+                    ) + "% done"}
+                </p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>No trainings match your search.</p>
+        )}
+      </section>
+    </>
+  );
 };
 
 export default EmployeeDashboard;
